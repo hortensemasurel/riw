ec5c7f9b60eb7a81f488421da9ae1a6c62a4d771
Astings <alexandre.laine@student.ecp.fr>
2020-04-09 16:09:33 +0200

    Adds first draft of interface

---
+++ main.py
@@ -0,0 +1,51 @@
+import time
+from nltk.stem import WordNetLemmatizer
+from models.search_engine import SearchEngine
+
+
+def clt_interface():
+    print("Loading search engine :\n")
+    word_net_lemmatizer = WordNetLemmatizer()
+    search_engine = SearchEngine(
+        collection_name="cs276",
+        stopwords_list=[],
+        lemmatizer=word_net_lemmatizer,
+        weighting_model=weighting_model,
+    )
+    print("Done !\n")
+
+    def search(query):
+        """
+        This function is the main interface for querying the search engine.
+        """
+        start_time = time.time()
+        print("Searching\n")
+        doc_scores_query = search_engine.search(query)
+        print("Sorting\n")
+        sorted_docs = [
+            k
+            for k, v in sorted(
+                doc_scores_query.items(), key=lambda item: item[1], reverse=True
+            )
+        ]
+
+        finished_time = time.time()
+        total_time = round((finished_time - start_time) * 1000, 2)
+        print("Found requested queries in {}ms".format(total_time))
+
+        print("Results : \n")
+
+        for i, doc_id in enumerate(sorted_docs[:10]):
+            document = search_engine.collection.documents[doc_id]
+            print("{}.\t{}/{}".format(i, document.folder, document.url))
+            print("\t{}\n".format(" ".join(document.key_words)))
+
+    while True:
+
+        print("Please enter your query : ")
+        user_query = input()
+        search(user_query)
+
+
+if __name__ == "__main__":
+    clt_interface()
